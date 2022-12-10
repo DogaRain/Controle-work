7cdb9930c581fc8e9e69af3eac6459374c30cd37
Eduard <eduard_mayburov@mail.ru>
2022-12-10 18:13:39 +0700

    create method for rebuilding array

--- task/Program.cs
+++ task/Program.cs
@@ -2,3 +2,19 @@
 Console.WriteLine("Введите массив строк через пробел");
 var stroka = Console.ReadLine();
 array = stroka.Split(' ');
+
+string[] rebuildArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+        result[j] = array[i];
+        j++;
+        }
+    }
+    return result;
+    
+}
